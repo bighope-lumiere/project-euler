aeedddc69241c6e5b18163ec4eba226cd0783f60
bighope-lumiere <bighope.lumiere0916@gmail.com>
2020-05-04 06:31:18 +0900

    solve

--- src/001-099/p048.py
+++ src/001-099/p048.py
@@ -1,19 +1,8 @@
-"""
-数字が膨大にならないように INF = 10 ** 10 で割りながら計算するように注意する。
-"""
+MOD = 10**10
 
-
-def self_pow(n):
-    res = 1
-    for i in range(n):
-        res *= n
-        n %= INF
-    return res
-
-
-INF = 10 ** 10
 ans = 0
 for i in range(1, 1001):
-    ans += self_pow(i)
-    ans %= INF
+    ans += pow(i, i, MOD)
+    ans %= MOD
+
 print(ans)
